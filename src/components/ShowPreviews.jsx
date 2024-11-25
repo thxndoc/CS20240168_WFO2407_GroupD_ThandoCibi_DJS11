f2083bb9a4540bce4ced777b69840e19d65c51e3
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-25 12:30:20 +0200

    Set what should show at preview, error and loading states

--- src/components/ShowPreviews.jsx
+++ src/components/ShowPreviews.jsx
@@ -1,2 +1,47 @@
 import { useState, useEffect } from "react";
 import { fetchPreviewsData } from "../api";
+
+export default function ShowPreviews() {
+    const [previews, setPreviews] =useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        async function loadPreviews() {
+            setLoading(true)
+            try {
+                const data = await fetchPreviewsData()
+                setPreviews(data)
+            } catch (error) {
+                setError(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        loadPreviews();
+    },[])
+
+    if (loading) {
+        return <div className="loader"></div>
+    }
+
+    if (error) {
+        return <h1 aria-live="assertive">There was an error: {error.message}</h1>
+    }
+
+    return (
+        <div>
+            <h1 className>All shows</h1>
+            <ul>
+                {previews.map((preview) => (
+                <li key={preview.id}>
+                <img src={preview.image} alt={preview.title} />
+                <h2>{preview.title}</h2>
+                <p>{preview.genres.join(", ")}</p>
+                <p>Last updated: {new Date(preview.updated).toLocaleDateString()}</p>
+                </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
