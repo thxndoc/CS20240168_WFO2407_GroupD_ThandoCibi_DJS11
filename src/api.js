de770219622bdc40c093ee53480ee7a7dd3af41d
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-25 10:26:26 +0200

    Create function that fetches genre by ID

--- src/api.js
+++ src/api.js
@@ -9,4 +9,13 @@ export async function fetchPreviewsData() {
     return data
 }
 
+// fetch genre by Id
+export async function fetchGenreById(genreId) {
+    if (!genreId) throw new Error('Genre ID is required')
+    const response = await fetch(`${baseUrl}/genre/${genreId}`)
+    if (!response.ok) throw new Error('Failed to fetch genre data')
+    const data = await response.json()
+    return data
+}
+
 
