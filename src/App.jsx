1e558cc862ee31e99a0a505ac11261d9936445ee
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-26 15:09:44 +0200

    Import Layout and add to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import ShowPreviews from './components/ShowPreviews'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Layout from './components/Layout'
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-            
-            <Route index element={<ShowPreviews />} />
+            <Route path="/" element={<Layout />}>
+                <Route index element={<ShowPreviews />} />
+            </Route>
         </Routes>
     </BrowserRouter>
   )
