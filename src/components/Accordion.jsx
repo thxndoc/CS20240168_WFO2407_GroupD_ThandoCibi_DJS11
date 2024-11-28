6bb3c49cd991a2ac670585a8a47b8a3fea422b3b
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-28 13:19:21 +0200

    Wrap title and subtitle in div

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -3,13 +3,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlusSquare } from "@fortawesome/free-regular-svg-icons/faPlusSquare";
 import { faMinusSquare } from "@fortawesome/free-regular-svg-icons/faMinusSquare";
 
-export default function Accordion({ title, content }) {
+export default function Accordion({ title, episodesInSeason, content }) {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
         <div className="accordion">
             <div className="accordion-header" onClick={() => setIsOpen(!isOpen)}>
-                <h3>{title}</h3>
+                <div>
+                    <h3>{title}</h3>
+                    <h4>{episodesInSeason}</h4>
+                </div>
+                
                 <button className="toggle-button">
                     {isOpen ? 
                     <FontAwesomeIcon icon={faMinusSquare}/> : 
