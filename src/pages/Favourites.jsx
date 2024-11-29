e39500b5bc16457732f733dd337e591d1ecef0ee
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-29 08:53:04 +0200

    Get favourited shows from local storage and enable removing of shows from favourites

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -3,9 +3,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as solidHeart, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default function Favourites() {
-    const [Favourites, setFavourites] = useState([])
+    const [favourites, setFavourites] = useState([])
 
     useEffect(()=> {
+        const favouritedShows = JSON.parse(localStorage.getItem('favourites')) || [];
+        setFavourites(favouritedShows);
+    }, [])
 
-    })
+    const removeFromFavourites = (showId) => {
+        const updatedFavourites = favourites.filter(show => show.id !== showId);
+        setFavourites(updatedFavourites);
+        localStorage.setItem('favourites', JSON.stringify(updatedFavourites));
+    };
+
+    if (favourites.length === 0) {
+        return (
+            <div className="favourites-page">
+                <h1>No Favourited Shows Yet...</h1>
+            </div>
+        );
+    }
 }
