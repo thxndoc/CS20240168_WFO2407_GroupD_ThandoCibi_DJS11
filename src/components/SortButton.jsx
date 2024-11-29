5f830b40f785eb3ff096f97cd1882f3d5ebab62b
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-29 14:13:34 +0200

    Edit displayed text for sort button -  most recently and least recently updated

--- src/components/SortButton.jsx
+++ src/components/SortButton.jsx
@@ -15,8 +15,8 @@ export default function SortButton({ onSort }) {
             <ul className="dropdown">
                 <li onClick={() => handleSorting("A-Z")}>A-Z</li>
                 <li onClick={() => handleSorting("Z-A")}>Z-A</li>
-                <li onClick={() => handleSorting("recentlyUpdated")}>Recently Updated</li>
-                <li onClick={() => handleSorting("leastRecentlyUpdated")}>Least Recently Updated</li>
+                <li onClick={() => handleSorting("recentlyUpdated")}>Newest</li>
+                <li onClick={() => handleSorting("leastRecentlyUpdated")}>Oldest</li>
             </ul>
         )}
     </div>
