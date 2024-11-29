e091d4c3a9413b600ea2a41d683253ba5d2e20ac
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-29 08:45:49 +0200

    Set up Favourites component with state and FontAwesome icons for heart and trash

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as solidHeart, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
-const [Favourites, setFavourites] = useState([])
+export default function Favourites() {
+    const [Favourites, setFavourites] = useState([])
 
-useEffect(()=> {
-    
-})
+    useEffect(()=> {
+
+    })
+}
