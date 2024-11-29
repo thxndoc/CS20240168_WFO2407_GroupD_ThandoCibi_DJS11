aadbfc2596a5b30e6dea82ec6fd4029986d9af5e
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-29 14:07:21 +0200

    Add route to favourites

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import ShowPreviews from './pages/ShowPreviews'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Layout from './components/Layout'
 import ShowDetails from './pages/ShowDetails'
+import Favourites from './pages/Favourites'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
             <Route path='/' element={<Layout />}>
                 <Route index element={<ShowPreviews />} />
                 <Route path="/:showId" element={<ShowDetails />} />
-                {/* <Route path="/favourites" element={<Favourites />} /> */}
+                <Route path="/favourites" element={<Favourites />} />
             </Route>
         </Routes>
     </BrowserRouter>
